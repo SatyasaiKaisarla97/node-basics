d0cc061926d89041ecbdeab513bb698bf5c5f6df
Satyasai <satyasai1997@gmail.com>
2023-12-08 15:03:47 +0530

    req-res-server

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Satyasai Kaisarla</h1>');
-});
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers);
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h1>Satyasai Kaisarla</h1>');
+// });
 
-server.listen(4000);
+// server.listen(4000);
+
+const serverTwo = http.createServer((req,res) => {
+    if(req.url === '/'){
+        res.write('<h1>Hello!</h1>')
+    }
+    if(req.url === '/home'){
+        res.write('<h1>Welcome Home</h1>')
+    }
+    if(req.url === '/about'){
+        res.write('<h1>Welcome to About Us Page</h1>')
+    }
+    if(req.url === '/node'){
+        res.write('<h1>Welcome to my Node.js Project</h1>')
+    }
+    res.end()
+})
+
+serverTwo.listen(3000)
